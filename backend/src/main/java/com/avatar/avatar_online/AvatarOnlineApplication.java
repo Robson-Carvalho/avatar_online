06484304901d03a826224ffc05a4f43c621d7816
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-09-26 09:42:17 -0300

    chore: initial Spring Boot project setup

---
+++ backend/src/main/java/com/avatar/avatar_online/AvatarOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.avatar.avatar_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvatarOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvatarOnlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/avatar/avatar_online/AvatarOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avatar.avatar_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvatarOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
