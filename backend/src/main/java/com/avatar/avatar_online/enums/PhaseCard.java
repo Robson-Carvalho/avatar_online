7124c930742c6b6b3f96656a15dc0252d6dbaef8
Guilherme Fernandes <guiaulas2020@gmail.com>
2025-09-27 13:10:50 -0300

    chore:Trazendo Enums para novo projeto

---
+++ backend/src/main/java/com/avatar/avatar_online/enums/PhaseCard.java
@@ -0,0 +1,8 @@
+package com.avatar.avatar_online.enums;
+
+public enum PhaseCard {
+    COMMON,   // Comum
+    YOUNG,    // Jovem
+    ADULT,    // Adulto
+    MASTER    // Mestre
+}
---
+++ backend/src/main/java/com/avatar/avatar_online/enums/RarityCard.java
@@ -0,0 +1,8 @@
+package com.avatar.avatar_online.enums;
+
+public enum RarityCard {
+    COMMON,   // Comum
+    RARE,     // Raro
+    EPIC,     // Épico
+    LEGENDARY // Lendário
+}
