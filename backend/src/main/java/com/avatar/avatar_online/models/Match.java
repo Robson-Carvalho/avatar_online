4c95b8df796657901dc9836900728c7cf2639424
Guilherme Fernandes <guiaulas2020@gmail.com>
2025-09-27 13:15:02 -0300

    chore:Trazendo Models para novo projeto

---
+++ backend/src/main/java/com/avatar/avatar_online/models/Match.java
@@ -0,0 +1,25 @@
+package com.avatar.avatar_online.models;
+
+import java.util.UUID;
+
+public class Match {
+    private String id;
+    private String playerOneID;
+    private String playerTwoID;
+    private String playerWin;
+
+
+    public Match() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {return this.id;}
+    public String getPlayerOneID() {return this.playerOneID;}
+    public String getPlayerTwoID() {return this.playerTwoID;}
+    public String getPlayerWin() {return this.playerWin;}
+
+    public void setId(String id) {this.id = id;}
+    public void setPlayerOneID(String playerOneID) {this.playerOneID = playerOneID;}
+    public void setPlayerTwoID(String playerTwoID) {this.playerTwoID = playerTwoID;}
+    public void setPlayerWin(String playerWin) {this.playerWin = playerWin;}
+}
