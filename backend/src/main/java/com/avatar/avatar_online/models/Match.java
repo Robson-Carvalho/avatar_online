22407999cad9a30bcf3348ac228d924855793ecd
Guilherme Fernandes <guiaulas2020@gmail.com>
2025-09-27 15:17:59 -0300

    refactor: atualizando models para modelo spring

--- backend/src/main/java/com/avatar/avatar_online/models/Match.java
+++ backend/src/main/java/com/avatar/avatar_online/models/Match.java
@@ -1,25 +1,23 @@
 package com.avatar.avatar_online.models;
 
-import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+
+@Entity
 public class Match {
-    private String id;
+    @Id
+    private Long id;
     private String playerOneID;
     private String playerTwoID;
     private String playerWin;
 
-
-    public Match() {
-        this.id = UUID.randomUUID().toString();
+    public Long getId() {
+        return id;
     }
 
-    public String getId() {return this.id;}
-    public String getPlayerOneID() {return this.playerOneID;}
-    public String getPlayerTwoID() {return this.playerTwoID;}
-    public String getPlayerWin() {return this.playerWin;}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public void setId(String id) {this.id = id;}
-    public void setPlayerOneID(String playerOneID) {this.playerOneID = playerOneID;}
-    public void setPlayerTwoID(String playerTwoID) {this.playerTwoID = playerTwoID;}
-    public void setPlayerWin(String playerWin) {this.playerWin = playerWin;}
 }
--- backend/src/main/java/com/avatar/avatar_online/models/SystemCard.java
+++ backend/src/main/java/com/avatar/avatar_online/models/SystemCard.java
@@ -3,10 +3,13 @@ package com.avatar.avatar_online.models;
 import com.avatar.avatar_online.enums.ElementCard;
 import com.avatar.avatar_online.enums.PhaseCard;
 import com.avatar.avatar_online.enums.RarityCard;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
-import java.util.Map;
-
+@Entity
 public class SystemCard {
+    @Id
+    private Long id;
     private String name;
     private ElementCard element;
     private PhaseCard phase;
@@ -16,94 +19,11 @@ public class SystemCard {
     private RarityCard rarity;
     private String description;
 
-    public SystemCard() {}
-
-    public SystemCard(String name, ElementCard element, PhaseCard phase, int attack, int life, int defense, RarityCard rarity, String description) {
-        this.name = name;
-        this.element = element;
-        this.phase = phase;
-        this.attack = attack;
-        this.life = life;
-        this.defense = defense;
-        this.rarity = rarity;
-        this.description = description;
+    public Long getId() {
+        return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ElementCard getElement() {
-        return element;
-    }
-
-    public void setElement(ElementCard element) {
-        this.element = element;
-    }
-
-    public PhaseCard getPhase() {
-        return phase;
-    }
-
-    public void setPhase(PhaseCard phase) {
-        this.phase = phase;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public void setAttack(int attack) {
-        this.attack = attack;
-    }
-
-    public int getLife() {
-        return life;
-    }
-
-    public void setLife(int life) {
-        this.life = life;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
-
-    public void setDefense(int defense) {
-        this.defense = defense;
-    }
-
-    public RarityCard getRarity() {
-        return rarity;
-    }
-
-    public void setRarity(RarityCard rarity) {
-        this.rarity = rarity;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-    public Map<String, Object> toJson() {
-        return Map.of(
-                "name", this.name,
-                "element", this.element,
-                "phase", this.phase,
-                "attack", this.attack,
-                "life", this.life,
-                "defense", this.defense,
-                "rarity", this.rarity,
-                "description", this.description
-        );
+    public void setId(Long id) {
+        this.id = id;
     }
 }
