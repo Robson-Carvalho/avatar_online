a739618db4068d83d9d5c63f79173b5c1cfe2f9a
DrizinCoder <guiaulas2020@gmail.com>
2025-09-29 13:25:28 -0300

    feat: Implementa base para pub-sub com kafaka

--- backend/src/main/java/com/avatar/avatar_online/pubsub/service/PublisherService.java
+++ backend/src/main/java/com/avatar/avatar_online/pubsub/service/PublisherService.java
@@ -1,5 +1,17 @@
 package com.avatar.avatar_online.pubsub.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PublisherService {
-    // Classe que atuará como publisher
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void publish(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
 }
--- backend/src/main/java/com/avatar/avatar_online/pubsub/service/SubscriverService.java
+++
@@ -1,5 +0,0 @@
-package com.avatar.avatar_online.pubsub.service;
-
-public class SubscriverService {
-    //Classe que atuará como Subscriber
-}
