3d9d719bd44fa61b6bbf045f2d4e7f321dba3b61
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-10-06 16:43:18 -0300

    refactor: adiciona métodos no UserRepository

--- backend/src/main/java/com/avatar/avatar_online/repository/UserRepository.java
+++ backend/src/main/java/com/avatar/avatar_online/repository/UserRepository.java
@@ -1,11 +1,26 @@
 package com.avatar.avatar_online.repository;
 
-import com.avatar.avatar_online.models.User;
+import com.avatar.avatar_online.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByNickname(String nickname);
+
+    @Query("SELECT u FROM app_user u WHERE u.email = :email OR u.nickname = :nickname")
+    Optional<UserEntity> findByEmailOrNickname(@Param("email") String email,
+                                               @Param("nickname") String nickname);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
