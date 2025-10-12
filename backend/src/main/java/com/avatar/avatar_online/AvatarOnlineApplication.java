8399bf73814830dd389294e5abe402b48eb14656
Guilherme Fernandes <guiaulas2020@gmail.com>
2025-10-12 20:40:46 -0300

    feat: Permite métodos executarem de forma assíncrona

--- backend/src/main/java/com/avatar/avatar_online/AvatarOnlineApplication.java
+++ backend/src/main/java/com/avatar/avatar_online/AvatarOnlineApplication.java
@@ -1,15 +1,19 @@
 package com.avatar.avatar_online;
 
+import com.avatar.avatar_online.raft.service.DatabaseSyncService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.xml.crypto.Data;
+
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class AvatarOnlineApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AvatarOnlineApplication.class, args);
-	}
-
+    public static void main(String[] args) {SpringApplication.run(AvatarOnlineApplication.class, args);}
 }
