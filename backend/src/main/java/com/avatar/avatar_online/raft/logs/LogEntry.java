4c483f7e156d6db5ea006ed7e24aa489d12cfee3
Guilherme Fernandes <guiaulas2020@gmail.com>
2025-10-15 17:34:41 -0300

    feat: Implementa LogEntry

---
+++ backend/src/main/java/com/avatar/avatar_online/raft/logs/LogEntry.java
@@ -0,0 +1,48 @@
+package com.avatar.avatar_online.raft.logs;
+
+public class LogEntry {
+
+    private long term;
+    private long index;
+    private Object command;
+    private boolean commited;
+
+    public LogEntry(long term, long index, Object command, boolean commited) {
+        this.term = term;
+        this.index = index;
+        this.command = command;
+        this.commited = commited;
+    }
+
+    public long getTerm() {
+        return term;
+    }
+
+    public void setTerm(long term) {
+        this.term = term;
+    }
+
+    public long getIndex() {
+        return index;
+    }
+
+    public void setIndex(long index) {
+        this.index = index;
+    }
+
+    public Object getCommand() {
+        return command;
+    }
+
+    public void setCommand(Object command) {
+        this.command = command;
+    }
+
+    public boolean isCommited() {
+        return commited;
+    }
+
+    public void setCommited(boolean commited) {
+        this.commited = commited;
+    }
+}
