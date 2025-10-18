c792aa7c891e7eb5c94be48cb3c224b2e71fc8da
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-10-18 00:13:41 -0300

    feat: adiciona comunicação publisher subscriber

---
+++ backend/src/main/java/com/avatar/avatar_online/publisher_subscriber/model/OperationMessage.java
@@ -0,0 +1,13 @@
+package com.avatar.avatar_online.publisher_subscriber.model;
+
+public class OperationMessage {
+    private String operation;
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+}
