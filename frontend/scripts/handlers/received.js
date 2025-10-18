f5110993e4aa0f657933c99eaabe74f3401dc5da
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-10-18 15:56:19 -0300

    feat: adiciona métodos para lidar com envio e recebimento de requisições

---
+++ frontend/scripts/handlers/received.js
@@ -0,0 +1,21 @@
+function handlerMain(message) {
+  const data = JSON.parse(message.body);
+
+  log(`📩 Resposta: messasge ${data.message}`);
+  log(`📩 Resposta: type: ${data.operationType}`);
+  log(`📩 Resposta: status: ${data.operationStatus}`);
+  log(`📩 Resposta: data: ${JSON.stringify(data.data)}`);
+
+  if (data.operationStatus == "ERROR") {
+    showError(`${data.message}`);
+  }
+
+  else if(data.operationStatus === "OK" &&  (data.operationType === "LOGIN_USER" || data.operationType === "CREATE_USER")) {
+    handleLoginUserOrCreateUserSuccess(data)
+  }
+}
+
+function handleLoginUserOrCreateUserSuccess(data) {
+  localStorage.setItem("user_avatar_online", JSON.stringify(data.data))
+  updateViewsBasedOnConnection()
+}
