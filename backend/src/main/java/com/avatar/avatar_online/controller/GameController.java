1923394429df5f1a5ef34a92733068bd182bce8a
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-10-22 12:05:40 -0300

    feat: adiciona game controller

---
+++ backend/src/main/java/com/avatar/avatar_online/controller/GameController.java
@@ -0,0 +1,11 @@
+package com.avatar.avatar_online.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/game")
+public class GameController {
+
+
+}
