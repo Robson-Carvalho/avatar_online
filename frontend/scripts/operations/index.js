4551e59ac7cc3bc97bcdce276c5dbe7570592943
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-11-25 21:36:09 -0300

    feat: adiciona chamada de método para fechar modal de loading de abertura de pacote

--- frontend/scripts/operations/index.js
+++ frontend/scripts/operations/index.js
@@ -1,89 +1,97 @@
 function handlePlayCard() {
-    const user = getUser()
-    const match = getMatch()
-    playCard(user.id, match.matchId);
+  const user = getUser();
+  const match = getMatch();
+  playCard(user.id, match.matchId);
 }
 
 function handleJoinInQueue() {
-    const user = getUser();
+  const user = getUser();
 
-    if (user != null)  {
-        JoinInQueue(user.id);
-    }
+  if (user != null) {
+    JoinInQueue(user.id);
+  }
 }
 
 function handleLogout() {
-    const user = getUser();
+  const user = getUser();
 
-    if (user != null)  {
-        logoutUser(user.id)
-    }
+  if (user != null) {
+    logoutUser(user.id);
+  }
 }
 
 function handleLogin(event) {
-    event.preventDefault();
-    
-    const nickname = document.getElementById('nickname-login').value;
-    const password = document.getElementById('password-login').value;
-    
-    console.log('Dados do login:', { nickname, password });
-    
-    loginUser(nickname, password);
+  event.preventDefault();
+
+  const nickname = document.getElementById("nickname-login").value;
+  const password = document.getElementById("password-login").value;
+
+  console.log("Dados do login:", { nickname, password });
+
+  loginUser(nickname, password);
 }
 
 function handleRegister(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.getElementById('name-register').value;
-    const nickname = document.getElementById('nickname-register').value;
-    const email = document.getElementById('email-register').value;
-    const password = document.getElementById('password-register').value;
-      
-    console.log('Dados de registro:', { name, nickname, email, password });
+  const name = document.getElementById("name-register").value;
+  const nickname = document.getElementById("nickname-register").value;
+  const email = document.getElementById("email-register").value;
+  const password = document.getElementById("password-register").value;
 
-    if (!name || !nickname || !email || !password) {
-        showWarning("Complete todos os campos!");
-        return;
-    }
-  
-    createUser(name, nickname, email, password);
+  console.log("Dados de registro:", { name, nickname, email, password });
+
+  if (!name || !nickname || !email || !password) {
+    showWarning("Complete todos os campos!");
+    return;
+  }
+
+  createUser(name, nickname, email, password);
 }
 
 function handleOpenPackage() {
-    const user = getUser()
+  const user = getUser();
 
-    if (user != null) {
-        openPackage(user.id)   
-    }
+  if (user != null) {
+    openPackage(user.id);
+  }
+
+  openLoadingOpenPack();
 }
 
-function handleGetDeck(){
-    const user = getUser()
+function handleGetDeck() {
+  const user = getUser();
 
-    if (user != null) {
-        getDeck(user.id)
-    }
+  if (user != null) {
+    getDeck(user.id);
+  }
 }
 
 function handleUpdateDeck() {
-    const user = getUser()
+  const user = getUser();
 
-    if (user != null) {
-        let cards = getDeckInfo()
-        updateDeck(user.id, cards[0].id, cards[1].id, cards[2].id, cards[3].id, cards[4].id)
-    }
-}   
-
-function handleGetCards() {
-    const user = getUser()
-
-    if (user != null) {
-        getCards(user.id)
-    }
+  if (user != null) {
+    let cards = getDeckInfo();
+    updateDeck(
+      user.id,
+      cards[0].id,
+      cards[1].id,
+      cards[2].id,
+      cards[3].id,
+      cards[4].id
+    );
+  }
 }
 
+function handleGetCards() {
+  const user = getUser();
+
+  if (user != null) {
+    getCards(user.id);
+  }
+}
 
 function logout() {
-    localStorage.removeItem('user_avatar_online');
-    updateViewsBasedOnConnection();
+  localStorage.removeItem("user_avatar_online");
+  updateViewsBasedOnConnection();
 }
