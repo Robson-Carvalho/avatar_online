0067478c0973d8de442bc65d19289c499cdc632d
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-11-25 22:51:01 -0300

    feat: adiciona visualização do endereço público

--- frontend/scripts/view/dashboard.js
+++ frontend/scripts/view/dashboard.js
@@ -3,9 +3,11 @@ function updateUserDisplay(data) {
 
   if (local != null) {
     document.getElementById("user_id").innerText = local.id;
+    document.getElementById("address_dash").innerText = local.address;
     document.getElementById("nickname_dash").innerText = local.nickname;
   }else{
     document.getElementById("user_id").innerText = data.id;
+    document.getElementById("address_dash").innerText = data.address;
     document.getElementById("nickname_dash").innerText = data.nickname;
   }
 }
